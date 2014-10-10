f01d778c5639095f79fd2544d6012b6e32b7640b
Jose Astudillo <jastu001@fiu.edu>
2014-10-10 00:19:31 -0400

    no message

---
+++ curriculumdb/protected/modules/catalog/views/prospective/index.php
@@ -0,0 +1,15 @@
+<?php
+/* @var $this ProspectiveController */
+
+$this->breadcrumbs=array(
+	'Prospective',
+);
+?>
+<h1>Prospective Catalog</h1>
+<!--<h1><?php echo $this->id . '/' . $this->action->id; ?></h1>-->
+
+<?php echo $this->renderPartial('menu'); ?>
+<p>
+	You may change the content of this page by modifying
+	the file <tt><?php echo __FILE__; ?></tt>.
+</p>

db50b3096fd01aef257e85c73a8437db3205335d
suttonc <chrisjsutton@hotmail.com>
2014-10-20 10:55:21 -0400

    no message

--- curriculumdb/protected/views/set/_links.php
+++ curriculumdb/protected/views/set/_links.php
@@ -1,3 +1,113 @@
+  <style>
+      .spacer {
+          position:relative;
+          float:left;
+          width:100%;
+      }
+	.box {
+	    position:relative;
+            width: 44%;
+            height: 45px;
+	    text-align: center;
+            border: solid black thin;
+	}
+        .box-left {
+	    float:left;
+        }
+	.box-right {
+            float:left;
+	}
+	.row {
+            position:relative;
+            float:left;
+		width:100%;
+		height:46px;
+	}
+        .outer {
+          position: relative;
+          float: left;
+          width: 750px;
+          border: thin solid red;
+      }
+  </style>
+  <script>
+    var isDragging = false;
+    var objSource = "";
+    var row = 0;
+  
+    function Box (id, dropState) {
+      this.id = id;
+      this.canDrop = dropState;
+    }
+    
+    function BoxRight (id) {
+      Box.call(this, id, true);
+    }
+    BoxRight.prototype = Object.create(Box.prototype);
+    BoxRight.prototype.constructor = BoxRight;
+    
+    function BoxLeft (id) {
+      Box.call(this, id, false);
+    }
+    BoxLeft.prototype = Object.create(Box.prototype);
+    BoxLeft.prototype.constructor = BoxLeft;
+    
+
+    var arrBox = new Array();
+    var arrBoxLeft = new Array();
+
+    
+	function allowDrop(obj, ev)
+	{
+		if (arrBox[obj.id].canDrop) {
+			ev.preventDefault();
+		}
+	}
+
+	function drag(parent, ev)
+	{
+		ev.dataTransfer.setData("Text",parent.id + ":" + ev.target.id);
+	}
+
+	function drop(obj, ev)
+	{   
+		if (arrBox[obj.id].canDrop) {
+			ev.preventDefault();
+			var dragData=ev.dataTransfer.getData("Text");
+                        var indexColon = dragData.indexOf(":");
+                        var sourceId = dragData.substring(0,indexColon);
+                        var dragId = dragData.substring(indexColon+1);
+			ev.target.appendChild(document.getElementById(dragId));
+                        var value = document.getElementById(dragId).innerHTML;
+                        
+                        if (obj.id.indexOf("boxRight") == 0) {
+                            var index = parseInt(obj.id.substring(8));
+                            document.getElementById("hidden"+index).value = value;
+                        }       
+                  
+                        if (sourceId.indexOf("boxRight") == 0) {
+                            var index = parseInt(sourceId.substring(8));
+                            document.getElementById("hidden"+index).value = "";
+                        }
+                        
+		}
+		arrBox[obj.id].canDrop = !arrBox[obj.id].canDrop;
+	}
+	
+	function dragStart(obj) {
+	    isDragging = true;
+	    objSource = obj;
+		objSource.style.border = "thin solid black";
+	}
+	
+	function dragEnd(obj) {
+	    if (isDragging) {
+			objSource.style.border = "thin solid red";
+			arrBox[objSource.id].canDrop = !arrBox[objSource.id].canDrop;
+		}
+		isDragging = false;
+	}
+</script>';
 <?php
 
 /**
@@ -5,7 +115,6 @@
  *  
  */
 //**************************** LIST OF GROUPS FOR MAJOR **************************************************
-
 $setByCourse = CurrSetByCourse::model()->with('course')->findAll('t.set_id=:id AND t.catalog_id=:catalogId', array(':id' => $id, 'catalogId' => $this->catalogId));
 if (empty($setByCourse)) {
 
@@ -23,35 +132,61 @@ if (empty($setByCourse)) {
     }
     echo '</ul>';
 }
-    echo '<table>';
-    echo '<th>Course ID</th><th>Course Name</th><th>Description</th><th>Credits</th><th>Pre-Requisites</th><th>Co-Requisites</th>';
+
+
+    $row = 0;
+    echo '<div class=\'outer\'>';
     foreach ($setByCourse AS $course)
-    {
+    {      
+        $string;
         $setByReq = HisRequisite::model()->with('requisite')->findAll('t.course_id=:cid', array(':cid' => $course->course_id));
-        echo '<tr>';
-        echo '<td>';
-             $entity = new Course($course->course_id, $this->catalogId); //$entity has current and history
-             $data = $entity->getHistoryEntity();    //extract history into $data, it has the course prefix id
-             $prefix = new CoursePrefix($data->coursePrefix_id, $this->catalogId); //prefix his and curr
-             echo $prefix->getHistoryEntity()->prefix; //extract the prefix from the history
-             echo ' '.$data->number; 
-        echo '</td>';
-        echo '<td>';
-        echo $course->course->name;
-        echo '</td>';
-        echo '<td>';
-            foreach($setByReq AS $req)
+        $entity = new Course($course->course_id, $this->catalogId); //$entity has current and history
+        $data = $entity->getHistoryEntity();    //extract history into $data, it has the course prefix id
+        $prefix = new CoursePrefix($data->coursePrefix_id, $this->catalogId); //prefix his and curr
+        $string = $prefix->getHistoryEntity()->prefix; //extract the prefix from the history
+        $string.= ' '.$data->number.'<br>';
+        $string.= $course->course->name.'<br>';
+        foreach($setByReq AS $req)
+        {
+            $entity1 = new Course($req->requisite_id, $this->catalogId);
+            $data1 = $entity1->getHistoryEntity();    //extract history into $data, it has the course prefix id
+            $prefix1 = new CoursePrefix($data1->coursePrefix_id, $this->catalogId); //prefix his and curr
+            if($req->level == 0)
             {
-                $entity1 = new Course($req->requisite_id, $this->catalogId);
-                $data1 = $entity1->getHistoryEntity();    //extract history into $data, it has the course prefix id
-                $prefix1 = new CoursePrefix($data1->coursePrefix_id, $this->catalogId); //prefix his and curr
-                echo $prefix1->getHistoryEntity()->prefix; //extract the prefix from the history
-                echo ' '.$data1->number; 
+                $string.= 'Pre: '.$prefix1->getHistoryEntity()->prefix; //extract the prefix from the history
+                $string.= ' '.$data1->number.' <br>'; 
+                break; //Flow chart to display only a single course as pre-req
             }
-        echo '</td>';
-        echo '</tr>';
+        }
+        foreach($setByReq AS $req)
+        {
+            $entity1 = new Course($req->requisite_id, $this->catalogId);
+            $data1 = $entity1->getHistoryEntity();    //extract history into $data, it has the course prefix id
+            $prefix1 = new CoursePrefix($data1->coursePrefix_id, $this->catalogId); //prefix his and curr
+            if($req->level == 1)
+            {
+                $string.= 'Co: '.$prefix1->getHistoryEntity()->prefix; //extract the prefix from the history
+                $string.= ' '.$data1->number.' <br>';
+                break; //Flow chart to display only a single course as co-req
+            }
+        }
+        
+        
+        
+        echo "<div id = row".$row ."class = row>'";
+        echo "<div id = boxLeft".$row ." class=\"box box-left\"";
+        echo $string;
+        
+    
+            
+        echo '<br></div>'; //Close Left
+        echo "<div id = boxRight".$row ." class=\"box box-right\">";
+            echo "Test";
+        echo '</div>';
+        echo '</div>'; //Close Row
+        $row = $row + 1;
     }
-    echo '</table>';
+    echo '</div>';    
 ?>
 <br/>
 
